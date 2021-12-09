53c6e24b228c5cffea99f50e62a3a1b6671aa26d
yanimastudio <88024103+yanimastudio@users.noreply.github.com>
2021-12-10 00:01:02 +0700

    fix https

--- script/jplayer.functions.js
+++ script/jplayer.functions.js
@@ -11,12 +11,12 @@ jQuery(document).ready(function() {
             title: "Bu Gan Shou (Couldn't say - ft. Yaongaku, Denis Kutepov) <small>Namusura</small> <span>05:44</span>",
             mp3: "music/Namu_song5_lamlai_withvocal.mp3",
             poster: "extra-images/banner-player-1.jpg",
-            oga: "http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg"
+            oga: "https://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg"
         }, {
             title: "Fu (Father - ft. Yaongaku, Denis Kutepov) <small>Namusura</small> <span>04:24</span>",
             mp3: "music/Namu_song2_lamlai_withvocal.mp3",
             poster: "extra-images/banner-player-2.jpg",
-            oga: "http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg"
+            oga: "https://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg"
         }
     ],
      {
@@ -57,32 +57,32 @@ jQuery(document).ready(function($) {
         //php writePlaylist('Y',"<span style='color:#828282'> (!!duration!!)</span>");
         {
             title: "Dj kozak-promo mix soundwave <span>03:08</span>",
-            mp3: "http://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
-            oga: "http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
+            mp3: "https://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
+            oga: "https://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
             poster: "extra-images/album-player-1.jpg",
             album_buy_mp3: "sdas",
             url: "sdas",
             layrics: "sdas",
         }, {
             title: "Shot me Down (feat. Skylar Grey) <span>02:56</span>",
-            mp3: "http://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
-            oga: "http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
+            mp3: "https://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
+            oga: "https://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
             poster: "extra-images/album-player-1.jpg",
             album_buy_mp3: "sdas",
             url: "sdas",
             layrics: "sdas",
         }, {
             title: "I'll Keep Loving you (feat. Birdy) <span>03:12</span>",
-            mp3: "http://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
-            oga: "http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
+            mp3: "https://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
+            oga: "https://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
             poster: "extra-images/album-player-1.jpg",
             album_buy_mp3: "sdas",
             url: "sdas",
             layrics: "sdas",
         }, {
             title: "The Hum (Short Edit) <span>03:37</span>",
-            mp3: "http://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
-            oga: "http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
+            mp3: "https://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
+            oga: "https://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
             poster: "extra-images/album-player-1.jpg",
             album_buy_mp3: "sdas",
             url: "sdas",
@@ -120,8 +120,8 @@ jQuery(document).ready(function() {
 
     var myCirclePlayer = new CirclePlayer("#jquery_jplayer_1",
     {
-        m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-        oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+        m4a: "https://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+        oga: "https://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
     }, {
         cssSelectorAncestor: "#cp_container_1",
         swfPath: "images/jplayer.swf",
@@ -131,8 +131,8 @@ jQuery(document).ready(function() {
 
     var myCirclePlayer = new CirclePlayer("#jquery_jplayer_2",
     {
-        m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-        oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+        m4a: "https://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+        oga: "https://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
     }, {
         cssSelectorAncestor: "#cp_container_2",
         swfPath: "images/jplayer.swf",
@@ -142,8 +142,8 @@ jQuery(document).ready(function() {
 
     var myCirclePlayer = new CirclePlayer("#jquery_jplayer_3",
     {
-        m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-        oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+        m4a: "https://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+        oga: "https://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
     }, {
         cssSelectorAncestor: "#cp_container_3",
         swfPath: "images/jplayer.swf",
@@ -153,8 +153,8 @@ jQuery(document).ready(function() {
 
     var myCirclePlayer = new CirclePlayer("#jquery_jplayer_4",
     {
-        m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-        oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+        m4a: "https://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+        oga: "https://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
     }, {
         cssSelectorAncestor: "#cp_container_4",
         swfPath: "images/jplayer.swf",
@@ -164,8 +164,8 @@ jQuery(document).ready(function() {
 
     var myCirclePlayer = new CirclePlayer("#jquery_jplayer_5",
     {
-        m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-        oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+        m4a: "https://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+        oga: "https://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
     }, {
         cssSelectorAncestor: "#cp_container_5",
         swfPath: "images/jplayer.swf",
@@ -175,8 +175,8 @@ jQuery(document).ready(function() {
 
     var myCirclePlayer = new CirclePlayer("#jquery_jplayer_6",
     {
-        m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-        oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+        m4a: "https://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+        oga: "https://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
     }, {
         cssSelectorAncestor: "#cp_container_6",
         swfPath: "images/jplayer.swf",
@@ -186,8 +186,8 @@ jQuery(document).ready(function() {
 
     var myCirclePlayer = new CirclePlayer("#jquery_jplayer_7",
     {
-        m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-        oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+        m4a: "https://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+        oga: "https://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
     }, {
         cssSelectorAncestor: "#cp_container_7",
         swfPath: "images/jplayer.swf",
@@ -197,8 +197,8 @@ jQuery(document).ready(function() {
 
     var myCirclePlayer = new CirclePlayer("#jquery_jplayer_8",
     {
-        m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-        oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+        m4a: "https://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+        oga: "https://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
     }, {
         cssSelectorAncestor: "#cp_container_8",
         swfPath: "images/jplayer.swf",
@@ -208,8 +208,8 @@ jQuery(document).ready(function() {
 
         var myCirclePlayer = new CirclePlayer("#jquery_jplayer_9",
         {
-            m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-            oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+            m4a: "https://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+            oga: "https://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
         }, {
             cssSelectorAncestor: "#cp_container_9",
             swfPath: "images/jplayer.swf",
